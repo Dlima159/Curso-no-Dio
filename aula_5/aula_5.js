1cb9b70dcddb21955aae13541674c3c48cf71598
Deivison Lima <deivisonjob159@gmail.com>
2022-09-29 17:12:05 -0300

    Manipulando Elementos da Página

---
+++ aula_5/aula_5.js
@@ -0,0 +1,17 @@
+function clicou(){
+    document.getElementById("agradecimentos").innerHTML="<b>Obrigado por clicar</b>";
+    window.location.href("https://developer.mozilla.org/en-US/");
+}
+
+function redirecionar(){
+    window.open("https://developer.mozilla.org/en-US/");
+
+}
+
+function detector(){
+    document.getElementById("detect").innerHTML="<b>Obrigado por olhar</b>";
+}
+
+function voltar(){
+    document.getElementById("detect").innerHTML="<b>Obrigado por passar o mouse</b>";
+}
