262cd1b87f55ee62a9a856e09b2875777e397be8
Deivison Lima <deivisonjob159@gmail.com>
2022-09-29 11:17:45 -0300

    uso de dicionários e listas

---
+++ aula_2/aula_2.js
@@ -0,0 +1,36 @@
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome: "uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var lista = ["maçã", "laranja", "pera"];
+// lista.push("uva","melancia");
+// lista.pop();
+// alert(lista.reverse());
+
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+
+
