c1c2798953876fb130bd08cf4fd623548b7e5ba2
Deivison Lima <deivisonjob159@gmail.com>
2022-09-30 16:42:55 -0300

    Manipulando Elementos da Página

--- aula_5/aula_5.js
+++ aula_5/aula_5.js
@@ -9,9 +9,29 @@ function redirecionar(){
 }
 
 function detector(){
-    document.getElementById("detect").innerHTML="<b>Obrigado por olhar</b>";
+    document.getElementById("detect").innerHTML="<b>Obrigado por passar o mouse</b>";
 }
 
 function voltar(){
-    document.getElementById("detect").innerHTML="<b>Obrigado por passar o mouse</b>";
+    document.getElementById("detect").innerHTML="<b>Passe o mouse aqui </b>";
+}
+
+function encontrar(element){
+    element.innerHTML="<b>Obrigado por passar o mouse</b>";
+}
+
+function retornar(element){
+    element.innerHTML="<b>Passe o mouse aqui</b>";
+}
+
+function pokemon(element){
+    if (element.value=="squirtle"){
+        document.getElementById("char").innerHTML="Parabéns por escolher um squirtle, ele é do tipo água e começa com bons status defensivos sendo excelente para a batalha no primeiro ginásio";       
+    }
+    else if (element.value=="Bulbasaur"){
+        document.getElementById("char").innerHTML="Parabéns por escolher um Bulbasaur, ele é do tipo grama/veneno e começa com status balanceados sendo bem efetivo no primeiro e excelente no segundo ginásio";       
+    }
+    else{
+        document.getElementById("char").innerHTML="Parabéns por escolher um Charmander, ele é do tipo fogo e começa com bons status ofensivos tendo desvantagem nos dois primeiros ginásios";       
+    }
 }
